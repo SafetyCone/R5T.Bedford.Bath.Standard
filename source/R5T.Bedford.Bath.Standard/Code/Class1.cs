f673cd1994fbbabd46934dba0515584055f9c7fc
MinexAutomation <minex.automation@gmail.com>
2020-01-25 01:53:59 -0800

    Added IServiceCollection extensions.

--- source/R5T.Bedford.Bath.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Bedford.Bath.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Bedford.Bath.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bath.Standard;
+
+
+namespace R5T.Bedford.Bath.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the binary-based <see cref="IFileEqualityComparer"/> service.
+        /// </summary>
+        public static IServiceCollection AddBinaryFileEqualityComparer(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IFileEqualityComparer, BinaryFileEqualityComparer>()
+                .AddHumanOutput()
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the text-based <see cref="IFileEqualityComparer"/> service.
+        /// </summary>
+        public static IServiceCollection AddTextFileEqualityComparer(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IFileEqualityComparer, TextFileEqualityComparer>()
+                .AddHumanOutput()
+                ;
+
+            return services;
+        }
+    }
+}
